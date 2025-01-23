fa9caa3b78cee4a7b1825539091ea56dd0c6404f
akashstwt <akashpatel115347@gmail.com>
2025-01-23 15:39:21 +0530

    authentication issue solved

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { createCivicAuthPlugin } from "@civic/auth/nextjs"
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withCivicAuth = createCivicAuthPlugin({
+  clientId: "1aa523d3-d80b-4b72-a081-523c5e1a0771"
+});
+
+export default withCivicAuth(nextConfig)
